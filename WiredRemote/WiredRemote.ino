4833a6a42a3e727b4974ca740d7abfca9497d3e5
Andras <andras.hernadi@ericsson.com>
2016-10-10 23:03:11 -0400

    Changing pind to match manufactured board

--- WiredRemote/WiredRemote.ino
+++ WiredRemote/WiredRemote.ino
@@ -1,8 +1,11 @@
-
 #include <SPI.h>
 
-byte address = 0x00;
-int CS= 10;
+byte address = 0x00; //wiper address
+int CS= 19;
+int wheelPin=A7; // steering wheel resistance reading pin. Strearing weheel goes between vcc and this pin
+int prevButton=0;
+
+
 // remote control commands for Pioneer
 //values are in ohms
 const int VOL_UP=16000;
@@ -12,23 +15,12 @@ const int NEXT_TR=8000;
 const int MODE=1200;
 const int MUTE=6;
 
-int csPin=10; // digipot pin
-
-int wheelPin=A0; // steering wheel resistance reading pin
-
-int i=0;
-int prevButton=0;
-
 void setup() {                
-  pinMode(csPin, OUTPUT);   
-  pinMode(13, OUTPUT);
+  pinMode(CS, OUTPUT);   
   delay(50);
-  blink(13, 3, 100);//bling 3 times adter boot
   SPI.begin();
   delay(50);
-  SPI.transfer(0); // command
-  SPI.transfer(0); // value
-  
+  digitalPotWrite(0); 
   pinMode(wheelPin, INPUT);
 
   delay(100);
@@ -59,16 +51,6 @@ int getR() { // this function returns pressed button code or 0 if all buttons ar
 //  if (r>=2 && r<=6) return(MUTE); 
   return (0);//below 0.58. we have a 10kOhm paralell resitor in the Honda//93=10kohm
 }
-void blink(int pin, int times, int delaytime)
-{
-  for (i=0; i<times; i++)
-  {
-       digitalWrite(pin, HIGH);
-      delay(delaytime);
-      digitalWrite(pin,LOW);
-            delay(delaytime);
-    }
-  }
 
 void loop() {
   int currButton=getR(); // get current pressed button code
@@ -76,11 +58,11 @@ void loop() {
     delay(10);
     currButton=getR(); // wait 10ms and read again to make sure this is not just some noise
     if (currButton!=prevButton) { 
-      Serial.println(currButton);
+      Serial.print("Output nominal resistance:");Serial.println(currButton);
       prevButton=currButton; 
       //button pressed. LEts bink
-      blink(13, 2, 100);
-      // send command to car stereo, below values are for JVC head units
+      //blink(13, 2, 100);
+      // send command to car stereo, below values are for PIONEER head units
       //SPI.transfer(0); //address byte. for 4131 the wiper's address is 0 
       switch(currButton) {//this determines the value sent to the above address round(expected resistance/(max resistance/wiper steps),0))
        case VOL_UP: digitalPotWrite(20); break;  // round(16000/(100000/127),0)
@@ -89,7 +71,7 @@ void loop() {
        case NEXT_TR: digitalPotWrite(10); break;  // round(8000/(100000/127),0)    
        case MODE: digitalPotWrite(2); break;  // round(1200/(100000/127),0)    
        //case MUTE: SPI.transfer(int (100000/127*MUTE)); break;  // 3k
-       default: digitalPotWrite(0);       blink(13, 1, 100);break; // nothing. Blink a second time on button release
+       default: digitalPotWrite(0);break; // nothing. 
      }
    }
   }
@@ -98,7 +80,7 @@ void loop() {
 
 void digitalPotWrite(int value)
 {
-  Serial.println(value);
+  Serial.print("Por command:"); Serial.println(value);
 digitalWrite(CS, LOW);
 SPI.transfer(address);
 SPI.transfer(value);
